67bb18c2d7f6925c0b14dd3f7374589ef68f0a3d
Dale B <daleban.dev@gmail.com>
2023-01-06 12:04:28 +0800

    chore: first section responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,42 +4,44 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main>
-      <section className='relative m-auto grid h-screen place-items-center'>
-        <h1 className='z-10 -translate-y-12 whitespace-nowrap text-center font-merchant-expanded text-8xl text-white'>
+      <section className='relative m-auto grid h-[60vh] place-items-center md:h-screen'>
+        <h1 className='text-mainTitle z-10 translate-y-24 whitespace-nowrap text-center font-merchant-expanded leading-none text-white'>
           Powered by Intellect, <br /> Driven by Values
         </h1>
         <Image
           src='/images/elements/gridsDotted.png'
           alt='Dotted Grids'
           fill
-          className='pointer-events-none -translate-y-96 object-contain object-top opacity-75'
+          className='pointer-events-none object-contain object-top opacity-75 xl:-translate-y-96'
         />
-        <Image
-          src='/images/gradients/gradientMain.png'
-          alt='Google Color Gradients'
-          fill
-          className='pointer-events-none object-contain object-bottom'
-        />
-        <Image
-          src='/images/elements/gdscLines.png'
-          alt='Google Color Lines'
-          fill
-          className='pointer-events-none z-[-1] translate-y-[30rem] object-contain object-bottom'
-        />
-        <Image
-          src='/images/elements/gdscLogoGlass.png'
-          alt='GDSC Logo Glass'
-          height={500}
-          width={1200}
-          className='pointer-events-none absolute bottom-0 -right-4 translate-y-1/2 scale-90'
-        />
-        <Image
-          src='/images/elements/arrowDown.png'
-          alt='Arrow Down'
-          height={300}
-          width={300}
-          className='pointer-events-none absolute left-32 bottom-48 translate-y-1/2 scale-[.80] mix-blend-overlay'
-        />
+        <div className=''>
+          <Image
+            src='/images/gradients/gradientMain.png'
+            alt='Google Color Gradients'
+            fill
+            className='pointer-events-none object-contain object-bottom'
+          />
+          <Image
+            src='/images/elements/gdscLines.png'
+            alt='Google Color Lines'
+            fill
+            className='pointer-events-none z-[-1] translate-y-56 object-contain object-bottom px-8 md:translate-y-[30rem]'
+          />
+          <Image
+            src='/images/elements/gdscLogoGlass.png'
+            alt='GDSC Logo Glass'
+            height={500}
+            width={1200}
+            className='pointer-events-none absolute bottom-0 -right-4 translate-y-1/2 scale-90'
+          />
+          <Image
+            src='/images/elements/arrowDown.png'
+            alt='Arrow Down'
+            height={300}
+            width={300}
+            className='pointer-events-none absolute -left-[5.5rem] bottom-28 translate-y-1/2 scale-[0.3] mix-blend-overlay sm:bottom-40 sm:-left-12 sm:scale-[0.5] md:bottom-48 xl:left-0 xl:scale-[0.7] 2xl:left-32 2xl:scale-[.80]'
+          />
+        </div>
       </section>
       <section className='relative overflow-x-hidden overflow-y-clip py-96 px-32 text-white'>
         <Image
@@ -53,7 +55,7 @@ export default function Home() {
           src='/images/elements/gridsDotted.png'
           alt='Dotted Grids'
           fill
-          className='pointer-events-none z-[-2]  translate-y-1/3 object-contain object-top opacity-75'
+          className='pointer-events-none z-[-2] translate-y-1/3 object-contain object-top opacity-80'
         />
         <div className='mx-auto mb-40 w-3/4'>
           <h2 className='mr-auto mb-10 font-merchant-expanded text-7xl'>
