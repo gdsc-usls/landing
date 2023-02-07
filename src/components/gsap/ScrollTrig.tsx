a78dfc063304c2042663d4486598237e124d8dc6
Dale B <daleban.dev@gmail.com>
2023-02-07 14:25:35 +0800

    feat: about page on scroll reveal animation

--- src/components/gsap/ScrollTrig.tsx
+++ src/components/gsap/ScrollTrig.tsx
@@ -6,6 +6,7 @@ import useLayoutEffect from '../hooks/use-isomorphic-layout-effect';
 const HandScrollAnimation = ({ children }: { children: React.ReactNode }) => {
   gsap.registerPlugin(ScrollTrigger);
 
+  // Fn for creating a new scrollTrigger instance
   function scrollTrig(trigger: string, start: string, scrub: number | boolean) {
     const tl = gsap.timeline({
       scrollTrigger: {
@@ -19,12 +20,20 @@ const HandScrollAnimation = ({ children }: { children: React.ReactNode }) => {
     return tl;
   }
 
-  useLayoutEffect(() => {
-    /**
-     * TL1
-     */
+  // Responsible for onScroll (intersection) animation
+  function scrollTrigText(trigger: string, target: string, start?: string) {
+    gsap.to(target, {
+      scrollTrigger: {
+        trigger: trigger as string,
+        start: start || 'center 75%',
+        // markers: true,
+      },
+      opacity: 1,
+      duration: 0.8,
+    });
+  }
 
-    const tl = scrollTrig('#about-gdsc-title', 'center 75%', false);
+  useLayoutEffect(() => {
     const heroTl = scrollTrig('#hero-section', 'bottom bottom', 0.7);
 
     heroTl
@@ -39,17 +48,39 @@ const HandScrollAnimation = ({ children }: { children: React.ReactNode }) => {
         '<'
       );
 
-    tl.to('#about-gdsc-title', {
-      opacity: 1,
-      duration: 0.8,
-    }).to(
-      '#about-gdsc-paragraph',
+    // Responsible for onScroll (intersection) animation
+    const textsTarget = [
       {
-        opacity: 1,
-        duration: 0.8,
+        trigger: '#about-gdsc-title',
       },
-      '<25%'
-    );
+      {
+        trigger: '#about-gdsc-paragraph',
+      },
+      {
+        trigger: '#usls-title-contain',
+        target: '#usls-title',
+        start: 'top 75%',
+      },
+      {
+        trigger: '#usls-title-contain',
+        target: '#usls-title-p',
+        start: 'top 75%',
+      },
+      {
+        trigger: '#gdsc-usls-title',
+      },
+      {
+        trigger: '#gdsc-usls-title-p',
+      },
+    ];
+
+    textsTarget.forEach((text) => {
+      scrollTrigText(
+        text.trigger,
+        text.target ? text.target : text.trigger,
+        text.start
+      );
+    });
   }, []);
 
   return <div>{children}</div>;
