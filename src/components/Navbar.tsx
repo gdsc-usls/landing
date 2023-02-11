10ed3751fc432b42bee3c80c5be7904fc20c9ba8
Dale B <daleban.dev@gmail.com>
2023-02-11 18:57:59 +0800

    feat: navbar isOpen state action

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,11 @@
 /* eslint-disable no-param-reassign */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Navbar = () => {
-  const navText = ['About Us', 'Projects', 'Events', 'Contact Us', 'Join Us!'];
+  // const navText = ['About Us', 'Projects', 'Events', 'Contact Us', 'Join Us!'];
 
   useEffect(() => {
     const menuItems = [...(document.querySelectorAll('.menu-item') as any)];
-    console.log(menuItems);
     menuItems.forEach((item: any) => {
       const word = item.children[0].innerText.split('');
       item.children[0].innerHTML = '';
@@ -22,12 +21,35 @@ export const Navbar = () => {
     });
   }, []);
 
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className='fixed left-0 right-0 z-40 m-auto mt-10 flex max-w-screen-2xl justify-between px-7 text-white mix-blend-difference md:px-14 lg:items-center'>
       <span className='logo font-merchant-expanded text-xl opacity-0'>
         GDSC USLS
       </span>
-      <div className='menu flex flex-col justify-center space-y-1 lg:flex-row lg:space-y-0  lg:space-x-10'>
+
+      <button
+        type='button'
+        className='group flex w-8 cursor-pointer flex-col items-center justify-center space-y-1 [&>span]:block [&>span]:h-[1.5px] [&>span]:w-full [&>span]:transform [&>span]:bg-white [&>span]:transition [&>span]:duration-300'
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span
+          className={`${
+            isOpen
+              ? 'translate-y-[3px] rotate-45 opacity-100 group-hover:opacity-50'
+              : 'opacity-100 group-hover:opacity-50'
+          }`}
+        />
+        <span
+          className={`${
+            isOpen
+              ? '-translate-y-[3px] -rotate-45 opacity-100 group-hover:opacity-50'
+              : 'opacity-100 group-hover:opacity-50'
+          }`}
+        />
+      </button>
+      {/* <div className='menu flex flex-col justify-center space-y-1 lg:flex-row lg:space-y-0  lg:space-x-10'>
         {navText.map((text) => (
           <a
             key={text}
@@ -41,7 +63,7 @@ export const Navbar = () => {
             </div>
           </a>
         ))}
-      </div>
+      </div> */}
     </nav>
   );
 };
