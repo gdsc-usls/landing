8fb80bef4c793bee4a395fe5aa68b13aad46b383
Josh Daniel <joshxfi.dev@gmail.com>
2023-03-12 12:08:13 +0800

    feat: setup google analytics

---
+++ src/config/firebase.ts
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+import { Analytics, getAnalytics } from 'firebase/analytics';
+
+// eslint-disable-next-line import/no-mutable-exports
+let analytics: Analytics;
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID!,
+};
+
+const app = initializeApp(firebaseConfig);
+
+if (typeof window !== 'undefined') {
+  analytics = getAnalytics(app);
+}
+
+export { app, analytics };
