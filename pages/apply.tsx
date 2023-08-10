efb9c640ab352193627ba2f58e7a0536001bd6fb
Josh Daniel <joshxfi.dev@gmail.com>
2023-08-10 22:20:59 +0800

    feat: show badge after register

--- pages/apply.tsx
+++ pages/apply.tsx
@@ -1,9 +1,11 @@
 import React, { FormEventHandler, useState } from 'react';
 import { gsap } from 'gsap';
-import useLayoutEffect from '~/hooks/useIsomorphicLayoutEffect';
-import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
-import { db } from '~/config/firebase';
 import { toast } from 'sonner';
+import Tilt from 'react-parallax-tilt';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
+
+import { db } from '~/config/firebase';
+import useLayoutEffect from '~/hooks/useIsomorphicLayoutEffect';
 
 type Member = {
   id: string;
@@ -17,6 +19,7 @@ export default function Apply() {
   const [lastName, setLastName] = useState('');
   const [studentId, setStudentId] = useState('');
   const [loading, setLoading] = useState(false);
+  const [isComplete, setIsComplete] = useState(false);
 
   useLayoutEffect(() => {
     gsap.to('body', {
@@ -60,54 +63,57 @@ export default function Apply() {
 
     toast.success('You are now a member!');
     setLoading(false);
-
-    setTimeout(() => {
-      setStudentId('');
-      setFirstName('');
-      setLastName('');
-    }, 500);
+    setIsComplete(true);
   };
 
   return (
-    <section className='mx-auto min-h-screen max-w-sm pt-40 font-googleSans-regular text-white'>
-      <h1 className='font-merchant-thin-condensed text-4xl'>Join GDSC—USLS!</h1>
+    <section className='px-6'>
+      {isComplete ? (
+        <Badge firstName={firstName} lastName={lastName} />
+      ) : (
+        <div className='mx-auto min-h-screen max-w-sm pt-40 font-googleSans-regular text-white'>
+          <h1 className='font-merchant-thin-condensed text-4xl'>
+            Join GDSC—USLS!
+          </h1>
 
-      <form onSubmit={handleAdd} className='mt-12 flex flex-col gap-y-4'>
-        <Input
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          label='First Name'
-          type='text'
-          maxLength={100}
-        />
-        <Input
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-          label='Last Name'
-          type='text'
-          maxLength={100}
-        />
-        <Input
-          value={studentId}
-          onChange={(e) => setStudentId(e.target.value)}
-          label='Student ID'
-          type='text'
-          maxLength={7}
-        />
+          <form onSubmit={handleAdd} className='mt-12 flex flex-col gap-y-4'>
+            <Input
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              label='First Name'
+              type='text'
+              maxLength={100}
+            />
+            <Input
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              label='Last Name'
+              type='text'
+              maxLength={100}
+            />
+            <Input
+              value={studentId}
+              onChange={(e) => setStudentId(e.target.value)}
+              label='Student ID'
+              type='text'
+              maxLength={7}
+            />
 
-        <div className='flex gap-x-2 text-sm text-gray-400'>
-          <input required type='checkbox' />
-          <p>All of the information I provided is true and correct.</p>
+            <div className='flex gap-x-2 text-sm text-gray-400'>
+              <input required type='checkbox' />
+              <p>All of the information I provided is true and correct.</p>
+            </div>
+
+            <button
+              type='submit'
+              disabled={loading}
+              className='h-[45px] rounded bg-white text-black'
+            >
+              {loading ? <span className='loader' /> : <p>Become a Member</p>}
+            </button>
+          </form>
         </div>
-
-        <button
-          type='submit'
-          disabled={loading}
-          className='h-[45px] rounded bg-white text-black'
-        >
-          {loading ? <span className='loader' /> : <p>Become a Member</p>}
-        </button>
-      </form>
+      )}
     </section>
   );
 }
@@ -134,3 +140,29 @@ const Input = ({
     </label>
   </div>
 );
+
+const Badge = ({
+  firstName,
+  lastName,
+}: {
+  firstName: string;
+  lastName: string;
+}) => {
+  return (
+    <div className='pt-52'>
+      <Tilt gyroscope className='mx-auto max-w-[400px]'>
+        <div className='relative grid w-full place-items-center'>
+          <div className='absolute mb-24 ml-16 w-[300px] font-googleSans-medium text-4xl text-gray-700'>
+            <p className='text-primary-100'>{firstName}</p>
+            <p className='text-primary-100'>{lastName}</p>
+          </div>
+          <img
+            className='pointer-events-none h-full w-full rounded object-contain'
+            src='/images/apply/badge.png'
+            alt='Appreciation Certificate'
+          />
+        </div>
+      </Tilt>
+    </div>
+  );
+};
